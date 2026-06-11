07447300a877ac33116b7aef8fe298f38eb3bca2
Manvija Cherukuri <88263337+Manvija02@users.noreply.github.com>
2026-06-11 13:55:34 -0400

    Fix formatting in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       {
         source: '/api/:path*',
         destination: `${process.env.BACKEND_URL || process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'}/api/:path*`,
+      },
     ];
   },
 };
